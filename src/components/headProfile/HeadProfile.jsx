8683101236fdcad5dbfeaf6715b7e9d35076ffcf
Endra Djati <endra.djati2004@gmail.com>
2024-09-28 00:06:25 +0700

    responsive update

--- src/components/headProfile/HeadProfile.jsx
+++ src/components/headProfile/HeadProfile.jsx
@@ -3,14 +3,14 @@ import './HeadProfile.css'
 
 const headProfile = () => {
   return (
-    <div className="portfolio">
+    <div className="headProfile">
       <div className="container">
         <div className="picName">
-          <img src="/endraPhoto.png" alt="" className='myselfPic'/>
+          <img src="/endraPhoto.png" alt="Endra Djati Pangestu" className='myselfPic'/>
           <div className="picDesc">
             <span className='introName'>Hello, My Name is...</span>
             <span className='myName'>Endra Djati Pangestu</span>
-            <span className='collegeName'>I am an Undergraduate Student <br /> at Binus University</span>
+            <span className='collegeName'>I'm an Undergraduate Student <br /> at Binus University</span>
           </div>
         </div>
       </div>
